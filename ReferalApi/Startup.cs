efc1ec79aa8ca08448186426bbad8e7f2025fcb5
goodwin <goodwinmcd@gmail.com>
2019-12-05 19:01:16 -0700

    added swagger docs

--- ReferalApi/Startup.cs
+++ ReferalApi/Startup.cs
@@ -1,19 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 using codingchallenge.ReferalApi.DataAccess;
 using codingchallenge.ReferalApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Npgsql;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ReferalApi
 {
@@ -33,6 +30,14 @@ namespace ReferalApi
             RegisterDI(services);
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Referrals API", Version = "v1" });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.XML";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         private void RegisterDI(IServiceCollection services)
@@ -51,6 +56,12 @@ namespace ReferalApi
             {
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
