56aca09535df138f5df9a22a6f9f8b5afbd02e2e
goodwin <goodwinmcd@gmail.com>
2019-12-08 19:41:42 -0700

    added unit test and wrote unit test for all get endpoints

---
+++ ReferralApiTests/Controllers/ReferralsControllerTest.cs
@@ -0,0 +1,126 @@
+using codingchallenge.ReferalApi.Models;
+using codingchallenge.ReferalApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ReferalApi.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace ReferralApiTests
+{
+    [TestClass]
+    public class ReferralsControllerTest
+    {
+        private Mock<IReferralCrudService> _mockReferralCrudService;
+        private ReferralsController _classUnderTest;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockReferralCrudService = new Mock<IReferralCrudService>();
+            _classUnderTest = new ReferralsController(_mockReferralCrudService.Object);
+        }
+
+        [TestMethod]
+        public void GetBadReferral_ShouldReturn404()
+        {
+            _mockReferralCrudService.Setup(m => m.GetReferral("fake"))
+                .Returns<string>(null);
+
+            var referral = _classUnderTest.GetReferral("fake");
+            var statusCode = (NotFoundObjectResult)referral.Result;
+
+            Assert.AreEqual(statusCode.StatusCode, (int)HttpStatusCode.NotFound);
+        }
+
+        [TestMethod]
+        public void GetReferral_ShouldReturn200()
+        {
+            var referralValue = "dummy";
+            var referral = new Referral
+            {
+                Title = referralValue,
+                ReferralCount = 0
+            };
+
+            _mockReferralCrudService.Setup(m => m.GetReferral(referralValue))
+                .Returns(referral);
+
+            var returnedReferral = _classUnderTest.GetReferral(referralValue);
+            var statusCode = returnedReferral.Result as OkObjectResult;
+            var finalResult = (Referral)statusCode.Value;
+
+            Assert.AreEqual(finalResult.Title, referralValue);
+        }
+
+        [TestMethod]
+        public void GetPagenatedReferralsWithArg_ReturnsAListOfReferrals()
+        {
+            var referralList = BuildReferralList();
+
+            _mockReferralCrudService.Setup(m => m.GetReferrals(1))
+                .Returns(referralList);
+
+            var returnedReferral = _classUnderTest.GetReferrals(1);
+            var statusCode = returnedReferral.Result as OkObjectResult;
+            var finalResult = (List<Referral>)statusCode.Value;
+
+            Assert.AreEqual(finalResult.Count, referralList.Count());
+        }
+
+        [TestMethod]
+        public void GetAllReferralsWithNoArgument_ReturnsAListOfReferrals()
+        {
+            var referralList = BuildReferralList();
+            _mockReferralCrudService.Setup(m => m.GetReferrals(It.IsAny<int>()))
+                .Returns(referralList);
+
+            var returnedReferral = _classUnderTest.GetReferrals();
+            var statusCode = returnedReferral.Result as OkObjectResult;
+            var finalResult = (List<Referral>)statusCode.Value;
+
+            Assert.AreEqual(finalResult.Count, referralList.Count());
+        }
+
+        [TestMethod]
+        public void GetAllReferralsWithNoReferrals_ReturnsEmptyList()
+        {
+            _mockReferralCrudService.Setup(m => m.GetReferrals(1))
+                .Returns<List<Referral>>(null);
+
+            var returnedReferral = _classUnderTest.GetReferrals();
+            var statusCode = returnedReferral.Result as OkObjectResult;
+            var finalResult = (List<Referral>)statusCode.Value;
+
+            Assert.AreEqual(finalResult.Count, 0);
+        }
+
+        private IEnumerable<Referral> BuildReferralList()
+        {
+            var referral1 = new Referral
+            {
+                Title = "referral1",
+                ReferralCount = 0,
+            };
+            var referral2 = new Referral
+            {
+                Title = "referral2",
+                ReferralCount = 0,
+            };
+            var referral3 = new Referral
+            {
+                Title = "referral3",
+                ReferralCount = 0,
+            };
+            var referralList = new List<Referral>
+            {
+                referral1,
+                referral2,
+                referral3,
+            };
+            return referralList;
+        }
+    }
+}
